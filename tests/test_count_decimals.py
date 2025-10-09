3c819818bd14a5c8d184f10eaa7c992be08b6859
Marius Kaffai <marius.kaffai.sowi@gmail.com>
2025-10-09 17:40:28 +0200

    add tests for _count_decimals()

---
+++ tests/test_count_decimals.py
@@ -0,0 +1,8 @@
+import simplebins
+
+def test_count_decimals():
+    assert simplebins.count_decimals(number=1) == 0
+    assert simplebins._count_decimals(number=1.0) == 0
+    assert simplebins._count_decimals(number=3.2) == 1
+    assert simplebins._count_decimals(number=0.05) == 2
+    assert simplebins._count_decimals(number=-1.02359) == 5
