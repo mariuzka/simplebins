585389fc6a718025e7c084f3a44baea8121c4086
Marius Kaffai <marius.kaffai.sowi@gmail.com>
2025-10-09 17:41:09 +0200

    fix typo

--- tests/test_count_decimals.py
+++ tests/test_count_decimals.py
@@ -1,7 +1,7 @@
 import simplebins
 
 def test_count_decimals():
-    assert simplebins.count_decimals(number=1) == 0
+    assert simplebins._count_decimals(number=1) == 0
     assert simplebins._count_decimals(number=1.0) == 0
     assert simplebins._count_decimals(number=3.2) == 1
     assert simplebins._count_decimals(number=0.05) == 2
