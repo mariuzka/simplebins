72fbbf8f7e16a7d50b50270d4591358aaa8b198c
Marius Kaffai <marius.kaffai.sowi@gmail.com>
2025-10-10 18:38:21 +0200

    add another test

--- tests/test_count_decimals.py
+++ tests/test_count_decimals.py
@@ -8,3 +8,4 @@ def test_count_decimals():
     assert simplebins._count_decimals(number=3.2) == 1
     assert simplebins._count_decimals(number=0.05) == 2
     assert simplebins._count_decimals(number=-1.02359) == 5
+    assert simplebins._count_decimals(number=0.4545000000000) == 4
